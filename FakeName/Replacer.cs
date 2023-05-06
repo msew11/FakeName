5b613ccb10010306e579ca995ae8efcb63af2b6e
秋水 <1123993881@qq.com>
2023-05-06 19:43:58 +0800

    fix: simplify it.

--- FakeName/Replacer.cs
+++ FakeName/Replacer.cs
@@ -120,15 +120,14 @@ internal static class Replacer
 
     private static bool ReplacePlayerName(this SeString text, string[] names, string replacement)
     {
-        var result = false;
         foreach (var name in names)
         {
             if(ReplacePlayerName(text, name, replacement))
             {
-                result = true;
+                return true;
             }
         }
-        return result;
+        return false;
     }
 
     private static bool ReplacePlayerName(this SeString text, string name, string replacement)
@@ -140,10 +139,12 @@ internal static class Replacer
         {
             if (payLoad is TextPayload load)
             {
+                if (string.IsNullOrEmpty(load.Text)) continue;
+
                 var t = load.Text.Replace(name, replacement);
                 if (t == load.Text) continue;
                 load.Text = t;
-                return true;
+                result = true;
             }
         }
         return result;
