6ff39adce5862ddb6ad8fae7e687491d5ec7e86d
msew11 <635481671@qq.com>
2024-04-15 04:04:57 +0800

    增加倒计时修改

--- FakeName/Plugin.cs
+++ FakeName/Plugin.cs
@@ -24,7 +24,7 @@ public class Plugin : IDalamudPlugin
     // internal ChatMessage ChatMessage { get; }
     
     internal DutyComponent DutyComponent { get; }
-    internal PartyListComponent PartyListComponent { get; }
+    internal HudComponent HudComponent { get; }
 
     internal WindowManager WindowManager { get; }
     // internal NameRepository NameRepository { get; }
@@ -42,7 +42,7 @@ public class Plugin : IDalamudPlugin
         this.Commands = new Commands(this);
 
         this.DutyComponent = new DutyComponent();
-        this.PartyListComponent = new PartyListComponent(Config);
+        this.HudComponent = new HudComponent(Config);
         //this.NameRepository = new NameRepository(this);
         
         //this.Common = new XivCommonBase(Hooks.NamePlates);
